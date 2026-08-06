4854799a7765caa49aa76c5be9dbfa39213ab91e
hijriyan <april@kuli.dev>
2026-08-06 16:56:16 +0700

    style: add bottom padding to TabsList in account-form to prevent border clipping

--- src/components/account-form.tsx
+++ src/components/account-form.tsx
@@ -120,7 +120,7 @@ export function AccountForm({ user, accounts, socialProviders }: AccountFormProp
       </div>
 
       <Tabs defaultValue="general" className="space-y-6">
-        <TabsList variant="line" className="h-auto w-full justify-start gap-6 bg-transparent p-0 border-b overflow-x-auto flex-nowrap whitespace-nowrap [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+        <TabsList variant="line" className="h-auto w-full justify-start gap-6 bg-transparent p-0 pb-[5px] border-b overflow-x-auto flex-nowrap whitespace-nowrap [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
           <TabsTrigger value="general" className="gap-2">
             <User className="size-4" />
             General
